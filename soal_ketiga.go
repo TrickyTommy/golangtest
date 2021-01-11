51cb7ef89e7bc63d4a729d53d618b78ccb727412
trickytommy <trickytommy4@gmail.com>
2021-01-11 22:42:32 +0700

    fix soal 3

--- soal_ketiga.go
+++ soal_ketiga.go
@@ -6,14 +6,22 @@ import (
 )
 
 func main() {
-	kalimat := "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras interdum mi eu magna fermentum, vel luctus tellus semper. Nunc dignissim eleifend ipsum, nec viverra mauris pellentesque non. Fusce auctor ex id mauris egestas, quis luctus nunc pharetra. Sed in dignissim nisi. Aliquam sed tempor urna, nec aliquam mi. Aenean eu feugiat lacus, vel dictum eros. Nulla condimentum porttitor aliquet. Vestibulum vehicula elit non arcu auctor maximus. Quisque est eros, maximus nec diam faucibus, mollis odio."
-	kalimat = strings.ToLower(kalimat)
-	res1 := strings.Count(kalimat, "a")
-	res2 := strings.Count(kalimat, "b")
-	res3 := strings.Count(kalimat, "z")
+	str := "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras interdum mi eu magna fermentum, vel luctus tellus semper. Nunc dignissim eleifend ipsum, nec viverra mauris pellentesque non. Fusce auctor ex id mauris egestas, quis luctus nunc pharetra. Sed in dignissim nisi. Aliquam sed tempor urna, nec aliquam mi. Aenean eu feugiat lacus, vel dictum eros. Nulla condimentum porttitor aliquet. Vestibulum vehicula elit non arcu auctor maximus. Quisque est eros, maximus nec diam faucibus, mollis odio."
 
-	fmt.Println("Karakter A sebanyak: ", res1)
-	fmt.Println("Karakter B sebanyak: ", res2)
-	fmt.Println("Karakter Z sebanyak: ", res3)
+	fmt.Println("Hitung Karakter A-Z")
+
+	for char := 'a'; char <= 'z'; char++ {
+		fmt.Print(string(char), "=", strings.Count(strings.ToLower(str), string(char)), ", ")
+	}
+
+	fmt.Println("\n\nGeser 5 karakter")
+
+	for _, char := range strings.ToLower(str) {
+		if int(char) >= 97 && int(char) <= 122 {
+			fmt.Print(string(char + 5))
+		} else {
+			fmt.Print(string(char))
+		}
+	}
 
 }
